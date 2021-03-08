d0771780403aed5ae9564552cff3a855639da9be
m1k1o <sedivy.miro@gmail.com>
2021-03-08 21:38:18 +0100

    add sweetalert & dark design.

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="indigo"
       dark
     >
       <div class="d-flex align-center">
@@ -39,6 +39,10 @@ export default Vue.extend({
 
   data: () => ({
     //
-  })
+  }),
+
+  created () {
+    this.$vuetify.theme.dark = true
+  }
 })
 </script>
