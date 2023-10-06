93c1e6fef8f1a13154ac45c5c18ba714153968d0
Miroslav Šedivý <sedivy.miro@gmail.com>
2023-10-06 21:49:35 +0200

    fix protected mount & export mounts.

--- internal/room/manager.go
+++ internal/room/manager.go
@@ -203,8 +203,16 @@ func (manager *RoomManagerCtx) ExportAsDockerCompose(ctx context.Context) ([]byt
 		}
 
 		// volumes
-		if len(containerJson.HostConfig.Binds) > 0 {
-			service["volumes"] = containerJson.HostConfig.Binds
+		volumes := []string{}
+		for _, mount := range container.Mounts {
+			if !mount.RW {
+				volumes = append(volumes, fmt.Sprintf("%s:%s:ro", mount.Source, mount.Destination))
+			} else {
+				volumes = append(volumes, fmt.Sprintf("%s:%s", mount.Source, mount.Destination))
+			}
+		}
+		if len(volumes) > 0 {
+			service["volumes"] = volumes
 		}
 
 		// devices
@@ -705,6 +713,8 @@ func (manager *RoomManagerCtx) GetSettings(ctx context.Context, id string) (*typ
 		} else if strings.HasPrefix(hostPath, templateStorageRoot) {
 			mountType = types.MountTemplate
 			hostPath = strings.TrimPrefix(hostPath, templateStorageRoot)
+		} else if !mount.RW {
+			mountType = types.MountProtected
 		}
 
 		mounts = append(mounts, types.RoomMount{
